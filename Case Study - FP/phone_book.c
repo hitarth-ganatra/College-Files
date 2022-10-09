a6be27c2d108be89f9760a19e30c34509ed313fa
Hitarth Ganatra <hitarthganatra123@gmail.com>
2022-10-09 20:41:52 +0530

    Modified Case Study

--- Case Study - FP/phone_book.c
+++ Case Study - FP/phone_book.c
@@ -2,28 +2,41 @@
 #include<stdlib.h>
 #include<string.h>
 #include<conio.h>
-#define MAX 10000 // Maximum number of contacts (can be increased)
+// #define MAX 10000 // Maximum number of contacts (can be increased)
 
-int max_num(){
-    FILE *list = fopen("list.txt", "ab++");
-    char buffer[MAX]; // buffer to store the data from the file
-    int j = 0; // j is used to check the next serial number of the contact 
-    double current, max = 0; //current stores the current serial number, max has the largest value of serial number in the file
+// int max_num(){
+//     FILE *list = fopen("list.txt", "ab++");
+//     char buffer[MAX]; // buffer to store the data from the file
+//     int j = 0; // j is used to check the next serial number of the contact 
+//     double current, max = 0; //current stores the current serial number, max has the largest value of serial number in the file
 
-    while(fgets(buffer, MAX, list) != NULL){
-        current = atof(buffer);
-        if(j == 0) max = current;
-        else if(current > max) max = current;
+//     while(fgets(buffer, MAX, list) != NULL){
+//         current = atof(buffer);
+//         if(j == 0) max = current;
+//         else if(current > max) max = current;
         
-        j++;
+//         j++;
+//     }
+//     fclose(list);
+//     return max;
+// }
+int main();
+void askagain(){
+    char choice;
+    printf("\n\n\tWould you like to do anything else? (y/n): ");
+    scanf("%c", &choice);
+    if(choice == 'y'){
+        main();
     }
-    fclose(list);
-    return max;
+    else{
+        printf("\n\tThank you for using the program.");
+    }
 }
 int add(){
     FILE *list = fopen("list.txt", "ab++");
     double number;
     char name[50];
+    char email[100];
 
     fflush(stdin);
     // To check the last serial number in the file
@@ -34,14 +47,24 @@ int add(){
         printf("Failed to create the required file.");
     }
     else{
+        printf("-----------------------------------------------------------------------------------------\n");
+        printf("\t   \xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB Add a number with name, number and email address \xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n\n");
         printf("Enter the full name of the contact: ");
         gets(name);
 
         printf("Enter the number of the contact: ");
         scanf("%lf", &number);
 
-        fprintf(list, "%s %.0lf\n", name, number);
+        fflush(stdin);
+        printf("Enter the email of the contact: ");
+        gets(email);
+
+        fprintf(list, "\n%s %.0lf %s\n", name, number, email);
         fclose(list);
+
+        printf("\nRecord added successfully.\n");
+        askagain();
+        printf("-----------------------------------------------------------------------------------------\n");
     }
 
     fclose(list);
@@ -58,10 +81,10 @@ int search(){
     double number, num_to_search;
     int error=0, flag = 0;
     int choice;
-    int mns;
-    int t = 0; 
+    char email[100];
+    int i = 1;
 
-    mns = max_num();
+    // mns = max_num();
 
     fflush(stdin);
 
@@ -70,22 +93,26 @@ int search(){
         printf("Failed to create the required file.");
     }
     else{
-        printf("\n\tWhat details do you would like to enter? ");
-        printf("\n\n\t1. Name");
-        printf("\n\t2. Number\n");
+        printf("-----------------------------------------------------------------------------------------\n");
+        printf("\t\t   \xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB Search any number from your Contacts \xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n\n");
+        printf("\n\t\t   What details do you would like to enter? ");
+        printf("\n\n\t\t\t\t   1. Name");
+        printf("\n\t\t\t\t   2. Email");
+        printf("\n\t\t\t\t   3. Number\n");
 
-        printf("\n\tEnter your choice: ");
+        printf("\n\t\t      Enter your choice: ");
         scanf("%d", &choice);
 
         
         switch (choice)
         {
             case 1:
-                printf("\tEnter the name of the contact to search: ");
+                fflush(stdin);
+                printf("\nEnter the name/surname of the contact to search: ");
                 gets(name_to_search);
 
-                printf("\n---------------------------------------------------------\n");         
-                while(fscanf(list, "%s %s %lf\n", name, surname, &number) != EOF){
+                printf("\n-----------------------------------------------------------------------------------------\n");         
+                while(fscanf(list, "%s %s %lf %s\n", name, surname, &number, email) != EOF){
 
                     result1 = strcmp(name_to_search, name);
                     result2 = strcmp(name_to_search, surname);
@@ -96,19 +123,22 @@ int search(){
                         error++;
                     }
                     while(flag != 0){
-                            printf("\n\t%s %s %.0lf", name, surname, number);
+                            printf("\n\t%d. %s %s - %.0lf - %s", i, name, surname, number, email);
                             flag--;
+                            i++;
                         }
-                }                
-                printf("\n---------------------------------------------------------\n");
-
+                }         
+                askagain();
+                printf("\n\n-----------------------------------------------------------------------------------------\n\n");
                 break;
             
-            case 2:
-                printf("Enter the number of the contact to search: ");
+            case 3:
+                fflush(stdin);
+                printf("\nEnter the number of the contact to search: ");
                 scanf("%lf", &num_to_search);
-
-                while(fscanf(list, "%s %s %lf\n", name, surname, &number) != EOF){
+                            
+                printf("\n-----------------------------------------------------------------------------------------\n");
+                while(fscanf(list, "%s %s %lf %s\n", name, surname, &number, email) != EOF){
                     if(number == num_to_search){
                         flag++;
                     }
@@ -116,19 +146,21 @@ int search(){
                         continue;
                     }
                     while(flag != 0){
-                            printf("\n---------------------------------------------------------\n");
-                            printf("\n\t%s %s %.0lf\n", name, surname, number);
+                            printf("\n\t%d. %s %s - %.0lf - %s", i, name, surname, number, email);
                             flag--;
-                        }
-                    printf("\n---------------------------------------------------------\n");
+                            i++;
+                    }
                 }
+                askagain();
+                printf("\n\n-----------------------------------------------------------------------------------------\n\n");
                 break;
 
             default:
-                printf("Invalid choice.");
+                printf("\n\tPlease enter a valid choice.");
+                askagain();
+                printf("\n-----------------------------------------------------------------------------------------\n\n");
                 break;
             }
-        
         }
 
     fclose(list);
@@ -173,12 +205,12 @@ int delete(){
                     else{
                         continue;
                     }
-                    printf("\n---------------------------------------------------------\n");
+                    printf("\n-----------------------------------------------------------------------------------------\n");
                     while(flag != 0){
                             
                             flag--;
                         }
-                    printf("\n---------------------------------------------------------\n");
+                    printf("\n-----------------------------------------------------------------------------------------\n");
                 }
                 break;
             case 2:
@@ -192,17 +224,17 @@ int delete(){
                     else{
                         continue;
                     }
-                    printf("\n---------------------------------------------------------\n");
+                    printf("\n-----------------------------------------------------------------------------------------\n");
                     while(flag != 0){
 
                             flag--;
                         }
-                    printf("\n---------------------------------------------------------\n");
+                    printf("\n-----------------------------------------------------------------------------------------\n");
                 }
                 break;
 
             default:
-                printf("Invalid choice.");
+                printf("\n\tPlease enter a valid choice.");
                 break;
         }
     }
@@ -260,20 +292,44 @@ void deletefun(){
 	// 	menu();
 	// };
 }
+void list(){
+    FILE *list = fopen("list.txt", "r");
+    char name[100];
+    char surname[100];
+    double number;
+    char email[100];
+    int i = 1;
 
+    printf("-----------------------------------------------------------------------------------------\n");
+    printf("\t\t\t\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB Contact List \xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n\n");
+    while (fscanf(list, "%s %s %lf %s\n", name, surname, &number, email) != EOF)
+    {
+        printf("\n\t\t%d. %s %s - %.0lf - %s", i, name, surname, number, email);
+        i++;
+    }
+    askagain();
+    printf("\n\n-----------------------------------------------------------------------------------------\n");
+}
+void edit(){
+    // FILE *list;
+    // list = fopen("list.txt","w");
+	// fclose(list);
+}
 int main()
 {
     int choice;
 
-    // system("cls");
-    system("color 60");
+    system("cls");
+    system("color B");
 
-    printf("***********************************************\n");
+    printf("***********************************************************\n");
     printf("\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB Welcome to the our Phone Book \xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB\n\n");
 
     printf("\t    1. Add a new contact\n");
     printf("\t    2. Search a contact\n");
-    printf("\t    3. Delete a contact\n\n");
+    printf("\t    3. Modify a contact\n");
+    printf("\t    4. List all contacts\n");
+    printf("\t    5. Delete a contact\n\n");
 
     printf("\t    Enter your choice: ");
     scanf("%d", &choice);
@@ -281,24 +337,32 @@ int main()
     switch (choice)
     {
         case 1:
-            printf("\n");            
+            system("cls");         
             add();
             break;
         case 2:
-            printf("\n");
+            system("cls");
             search();
             break;
         case 3:
-            printf("\n");
+            system("cls");
+            edit();
+            break;
+        case 4:
+            system("cls");
+            list();
+            break;
+        case 5:
+            system("cls");
             deletefun();
             break;
         default:
             printf("\n");
-            printf("\tPlease enter a valid choice.");
+            printf("\tPlease enter a valid choice.\n");
+            printf("***********************************************************\n");
             break;
     }
 
-    printf("\n***********************************************\n");
 
     return 0;
 }
